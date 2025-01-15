dbbdce4d727ed02ff3bee2cff412e3704e0ace6e
erezcohen <erez.cohen@gmail.com>
2025-01-15 21:04:24 +0200

    01

---
+++ anthropic_api_fundamentals/01_exe.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+my_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+from anthropic import Anthropic
+
+client = Anthropic(api_key=my_api_key)
+
+first_message = client.messages.create(
+    model="claude-3-haiku-20240307",
+    max_tokens=1000,
+    messages=[
+        {"role": "user", "content": "Hi there! Please tell me a joke"},
+    ],
+)
+
+print(first_message.content[0])
